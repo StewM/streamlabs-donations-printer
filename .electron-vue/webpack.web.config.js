e0fc3fb6d025b27544f9ad9b93390220c0678340
JMcB <joel.mcbride1@live.com>
2021-12-12 18:08:03 +0000

    Revert "Start fixing webpack to v5 from v4"
    This reverts commit 518c6eaa91ede804f2f992294c8d29b2113154f7.

--- .electron-vue/webpack.web.config.js
+++ .electron-vue/webpack.web.config.js
@@ -12,7 +12,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
 
 let webConfig = {
-  devtool: 'eval-cheap-module-source-map',
+  devtool: '#cheap-module-eval-source-map',
   entry: {
     web: path.join(__dirname, '../src/renderer/main.js')
   },
@@ -62,7 +62,7 @@ let webConfig = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         use: {
           loader: 'url-loader',
-          options: {
+          query: {
             limit: 10000,
             name: 'imgs/[name].[ext]'
           }
@@ -72,7 +72,7 @@ let webConfig = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         use: {
           loader: 'url-loader',
-          options: {
+          query: {
             limit: 10000,
             name: 'fonts/[name].[ext]'
           }
